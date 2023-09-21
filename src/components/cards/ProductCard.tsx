af97f6ceee16554b179d6effe5aa3ba0b86d2a08
rkeerthient <rkeerthi@yext.com>
2023-09-21 18:28:03 +0530

    added pop up to cards

--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -3,15 +3,31 @@
 import { CardComponent, CardProps } from "@yext/search-ui-react";
 import * as React from "react";
 import Product from "../../types/products";
+import InventoryModal from "../product-inventory-modal";
+import { useState } from "react";
 
 const ProductCard: CardComponent<Product> = ({
   result,
 }: CardProps<Product>): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const product = result.rawData;
   const hrefURL = "/" + product.slug;
+  const openDialog = () => {
+    setIsOpen(true);
+  };
 
+  const closeDialog = () => {
+    setIsOpen(false);
+  };
   return (
     <div>
+      <InventoryModal
+        productName={product.name}
+        stockAvailability={product.c_inStock}
+        isOpen={isOpen}
+        onClose={closeDialog}
+      ></InventoryModal>
       <div className="flex flex-col items-center p-4 text-center">
         <div className="rounded-full">
           <img
@@ -27,7 +43,12 @@ const ProductCard: CardComponent<Product> = ({
         >
           {product.name}
         </a>
-        <button className="text-xs text-brand-cta-hover italic underline">Check store inventory</button>
+        <button
+          onClick={openDialog}
+          className="text-xs text-brand-cta-hover italic underline"
+        >
+          Check store inventory
+        </button>
         <div>${product.c_cPrice}</div>
       </div>
     </div>
